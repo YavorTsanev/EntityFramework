ff6c267f27db7dc13ba1b40c351f294e45a2f423
Yavor Tsanev <69981325+YavorTsanev@users.noreply.github.com>
2021-01-27 17:55:51 +0200

    adding files

--- MiniORM/MiniORM/ChangeTracker.cs
+++ MiniORM/MiniORM/ChangeTracker.cs
@@ -1,11 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Reflection;
 
 namespace MiniORM
 {
-	internal class ChangeTracker<T> where T : class, new()
+    // TODO: Create your ChangeTracker class here.
+    internal class ChangeTracker<T>
+        where T : class, new()
     {
         private readonly List<T> allEntities;
 
@@ -15,38 +18,96 @@ namespace MiniORM
 
         public ChangeTracker(IEnumerable<T> entities)
         {
+            this.allEntities = CloneEntities(entities);
+
             this.added = new List<T>();
             this.removed = new List<T>();
-
-            this.allEntities = CloneEntities(entities);
         }
 
-        public IReadOnlyCollection<T> AllEntities => this.allEntities;
+        public IReadOnlyCollection<T> AllEntities
+            => this.allEntities.AsReadOnly();
 
-        public IReadOnlyCollection<T> Added => this.added;
+        public IReadOnlyCollection<T> Added
+             => this.added.AsReadOnly();
 
-        public IReadOnlyCollection<T> Removed => this.removed;
+        public IReadOnlyCollection<T> Removed
+            => this.removed.AsReadOnly();
+
+        public void Add(T item) => this.added.Add(item);
+
+        public void Remove(T item) => this.removed.Add(item);
 
         private static List<T> CloneEntities(IEnumerable<T> entities)
         {
             var clonedEntities = new List<T>();
 
-            var propertiesToClone = typeof(T).GetProperties().Where(pi => DbContext.AllowedSqlTypes.Contains(pi.PropertyType)).ToArray();
+            var propertiesToClone = typeof(T).GetProperties()
+                .Where(pi => DbContext.AllowedSqlTypes.Contains(pi.PropertyType))
+                .ToArray();
 
             foreach (var entity in entities)
             {
                 var clonedEntity = Activator.CreateInstance<T>();
 
-                foreach (PropertyInfo property in propertiesToClone)
+                foreach (var property in propertiesToClone)
                 {
                     var value = property.GetValue(entity);
-                    property.SetValue(clonedEntity,value);
+
+                    property.SetValue(clonedEntity, value);
                 }
 
                 clonedEntities.Add(clonedEntity);
             }
-             
+
             return clonedEntities;
+        }
+
+        private static bool IsModified(T proxyEntity, T entity)
+        {
+            var monitoredProperties = typeof(T).GetProperties()
+                .Where(pi => DbContext.AllowedSqlTypes.Contains(pi.PropertyType));
+
+            var modifiedProperties = monitoredProperties
+                .Where(pi => !Equals(pi.GetValue(proxyEntity), pi.GetValue(entity)))
+                .ToArray();
+
+            var IsModified = modifiedProperties.Any();
+
+            return IsModified;
+        }
+
+        private static IEnumerable<object> GetPrimaryKeyValues(IEnumerable<PropertyInfo> primaryKeys, T entity)
+        {
+            return primaryKeys.Select(pk => pk.GetValue(entity));
+        }
+
+
+        public IEnumerable<T> GetModifiedEntities(DbSet<T> dbSet)
+        {
+            var modifiedEntities = new List<T>();
+
+            var primaryKeys = typeof(T).GetProperties()
+                .Where(pi => pi.HasAttribute<KeyAttribute>())
+                .ToArray();
+
+            foreach (var proxyEntity in this.AllEntities)
+            {
+                var primaryKeyValues = GetPrimaryKeyValues(primaryKeys, proxyEntity)
+                    .ToArray();
+
+                var entity = dbSet
+                    .Entities
+                    .Single(e => GetPrimaryKeyValues(primaryKeys, e).SequenceEqual(primaryKeyValues));
+
+                var isModified = IsModified(proxyEntity, entity);
+
+                if (isModified)
+                {
+                    modifiedEntities.Add(entity);
+                }
+            }
+
+            return modifiedEntities;
         }
     }
 }
