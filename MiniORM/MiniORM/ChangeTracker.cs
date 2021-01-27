3bdb2d7c7cc9b12fe79ef7a95b4841797c37701d
Yavor Tsanev <69981325+YavorTsanev@users.noreply.github.com>
2021-01-27 12:20:42 +0200

    Add project files.

---
+++ MiniORM/MiniORM/ChangeTracker.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+
+namespace MiniORM
+{
+	internal class ChangeTracker<T> where T : class, new()
+    {
+        private readonly List<T> allEntities;
+
+        private readonly List<T> added;
+
+        private readonly List<T> removed;
+
+        public ChangeTracker(IEnumerable<T> entities)
+        {
+            this.added = new List<T>();
+            this.removed = new List<T>();
+
+            this.allEntities = CloneEntities(entities);
+        }
+
+        public IReadOnlyCollection<T> AllEntities => this.allEntities;
+
+        public IReadOnlyCollection<T> Added => this.added;
+
+        public IReadOnlyCollection<T> Removed => this.removed;
+
+        private static List<T> CloneEntities(IEnumerable<T> entities)
+        {
+            var clonedEntities = new List<T>();
+
+            var propertiesToClone = typeof(T).GetProperties().Where(pi => DbContext.AllowedSqlTypes.Contains(pi.PropertyType)).ToArray();
+
+            foreach (var entity in entities)
+            {
+                var clonedEntity = Activator.CreateInstance<T>();
+
+                foreach (PropertyInfo property in propertiesToClone)
+                {
+                    var value = property.GetValue(entity);
+                    property.SetValue(clonedEntity,value);
+                }
+
+                clonedEntities.Add(clonedEntity);
+            }
+
+            return clonedEntities;
+        }
+    }
+}
