fcbae44ac89dc536fd854137f463e8ee850517a0
Yavor Tsanev <69981325+YavorTsanev@users.noreply.github.com>
2021-01-28 17:27:07 +0200

    adding files

---
+++ Entity Framework Introduction/LAB/LAB/EFDemo/StartUp.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace EFDemo
+{
+    class StartUp
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
