e2be05ba29889d4841f95c42aa6870d9b0385bca
Yavor Tsanev <69981325+YavorTsanev@users.noreply.github.com>
2021-01-28 13:35:10 +0200

    refactoring

--- MiniORM/MiniORM.App/StartUp.cs
+++ MiniORM/MiniORM.App/StartUp.cs
@@ -1,29 +1,31 @@
 ﻿namespace MiniORM.App
 {
-	using System.Linq;
-	using Data;
-	using Data.Entities;
+    using System;
+    using System.Linq;
+    using Data;
+    using Data.Entities;
 
-	public class StartUp
-	{
-		public static void Main(string[] args)
-		{
-			var connectionString = "Server=.;Database=MiniORM;Integrated Security=True";
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            var connectionString = "Server=.;Database=MiniORM;Integrated Security=True";
 
-			var context = new SoftUniDbContext(connectionString);
+            var context = new SoftUniDbContext(connectionString);
 
-			context.Employees.Add(new Employee
-			{
-				FirstName = "Goshoooooo",
-				LastName = "Inserted",
-				DepartmentId = context.Departments.First().Id,
-				IsEmployed = true,
-			});
 
-			var employee = context.Employees.Last();
-			employee.FirstName = "Modified";
+            context.Employees.Add(new Employee
+            {
+                FirstName = "Gosho",
+                LastName = "Inserted",
+                DepartmentId = context.Departments.First().Id,
+                IsEmployed = true,
+            });
 
-			context.SaveChanges();
-		}
-	}
+            var employee = context.Employees.Last();
+            employee.FirstName = "Modified";
+
+            context.SaveChanges();
+        }
+    }
 }
