4cc844068f3dc0f99d4bac679abfe60264cf1861
Yavor Tsanev <69981325+YavorTsanev@users.noreply.github.com>
2021-02-01 12:24:28 +0200

    adding files

---
+++ Entity Framework Introduction/LAB/LAB/Code First/Models/RecipiesDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Code_First.Models
+{
+    class RecipiesDbContext : DbContext
+    {
+        public DbSet<Recipe> Recipes { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=.;Database=Recipes;Integrated Security=true;");
+        }
+
+    }
+}
---
+++ Entity Framework Introduction/LAB/LAB/Code First/StartUp.cs
@@ -0,0 +1,14 @@
+﻿using Code_First.Models;
+using System;
+
+namespace Code_First
+{
+    class StartUp
+    {
+        static void Main(string[] args)
+        {
+            var db = new RecipiesDbContext();
+            db.Database.EnsureCreated();
+        }
+    }
+}
