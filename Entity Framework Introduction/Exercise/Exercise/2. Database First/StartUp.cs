fbfd7249bc1e017721e23d8316e25ff8386e2f69
Yavor Tsanev <69981325+YavorTsanev@users.noreply.github.com>
2021-02-02 12:36:14 +0200

    added files

--- Entity Framework Introduction/Exercise/Exercise/2. Database First/StartUp.cs
+++ Entity Framework Introduction/Exercise/Exercise/2. Database First/StartUp.cs
@@ -1,6 +1,8 @@
 ﻿using SoftUni.Data;
+//Problem 2
 using System;
 using System.Linq;
+using System.Text;
 
 namespace SoftUni
 {
@@ -8,18 +10,56 @@ namespace SoftUni
     {
         static void Main()
         {
-            var db = new SoftUniContext();
-           // EmployeesFullInformation(db);
-        }
+            var context = new SoftUniContext();
+            //var result = EmployeesFullInformation(db);
 
-        private static void EmployeesFullInformation(SoftUniContext db)
+            //var result = GetEmployeesWithSalaryOver50000(db);
+
+            var result = GetEmployeesFromResearchAndDevelopment(context);
+
+            Console.WriteLine(result);
+        }
+        //Problem 3
+        private static string GetEmployeesWithSalaryOver50000(SoftUniContext context)
         {
-            var employees = db.Employees.Select(e => new { e.FirstName, e.LastName, e.MiddleName, e.JobTitle, e.Salary, e.EmployeeId }).OrderBy(x => x.EmployeeId);
+            var sb = new StringBuilder();
+
+            var employees = context.Employees.Where(e => e.Salary > 50000).Select(e => new { e.FirstName, e.Salary }).OrderBy(x => x.FirstName).ToList();
 
             foreach (var employee in employees)
             {
-                Console.WriteLine($"{employee.FirstName} {employee.LastName} {employee.MiddleName} {employee.JobTitle} {employee.Salary:f2}");
+                sb.AppendLine($"{employee.FirstName} {employee.Salary:f2}");
             }
+
+            return sb.ToString().TrimEnd();
+        }
+        //Problem 4
+        public static string EmployeesFullInformation(SoftUniContext context)
+        {
+            var sb = new StringBuilder();
+
+            var employees = context.Employees.Select(e => new { e.FirstName, e.LastName, e.MiddleName, e.JobTitle, e.Salary, e.EmployeeId }).OrderBy(x => x.EmployeeId).ToList();
+
+            foreach (var employee in employees)
+            {
+                sb.AppendLine($"{employee.FirstName} {employee.LastName} {employee.MiddleName} {employee.JobTitle} {employee.Salary:f2}");
+            }
+
+            return sb.ToString().TrimEnd();
+        }
+        //Problem 5
+        public static string GetEmployeesFromResearchAndDevelopment(SoftUniContext context)
+        {
+            var sb = new StringBuilder();
+
+            var employees = context.Employees.Where(e => e.Department.Name == "Research and Development").Select(e => new { e.FirstName, e.LastName, Depart = e.Department.Name, e.Salary}).OrderBy(e => e.Salary).ThenByDescending(e => e.FirstName).ToList();
+
+            foreach (var employee in employees)
+            {
+                sb.AppendLine($"{employee.FirstName} {employee.LastName} from {employee.Depart} - ${employee.Salary:f2}");
+            }
+
+            return sb.ToString().TrimEnd();
         }
     }
 }
