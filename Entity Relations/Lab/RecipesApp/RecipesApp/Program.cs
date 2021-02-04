f610968b69ebf66d97209b45b2fcb9d446e24625
Yavor Tsanev <69981325+YavorTsanev@users.noreply.github.com>
2021-02-04 11:05:54 +0200

    added files

---
+++ Entity Relations/Lab/RecipesApp/RecipesApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace RecipesApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
