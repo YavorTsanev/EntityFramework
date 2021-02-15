ff23698c31fe4b107127a1a66fdf14c786ceddf8
Yavor Tsanev <69981325+YavorTsanev@users.noreply.github.com>
2021-02-15 19:31:13 +0200

    add files

---
+++ Entity Relations/Exercise/Entity Relations/1. Student System/Data/StudentSystemContext .cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace P01_StudentSystem.Data
+{
+    public class StudentSystemContext : DbContext
+    {
+
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer("Server=.;Database=StudentSystem;Integrated Security=true;");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            
+        }
+    }
+
+   
+
+
+
+}
