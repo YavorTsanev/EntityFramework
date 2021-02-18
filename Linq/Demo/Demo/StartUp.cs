29a8b19c5ca90801ca1b4121afe518de6227a0f4
Yavor Tsanev <69981325+YavorTsanev@users.noreply.github.com>
2021-02-18 19:47:04 +0200

    adding new project

---
+++ Linq/Demo/Demo/StartUp.cs
@@ -0,0 +1,17 @@
+﻿using Demo.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Linq;
+
+namespace Demo
+{
+    class StartUp
+    {
+        static void Main(string[] args)
+        {
+            var db = new MusicXContext();
+
+            db.Songs.Include(s => s.SongMetadata).Take(20).Select(s => new { }).ToList().ForEach(s => Console.WriteLine(string.Join(", ",s)));
+        }
+    }
+}
