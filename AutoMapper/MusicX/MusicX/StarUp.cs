afd83e286172c08e67d8f39b02abf66a6652190c
Yavor Tsanev <69981325+YavorTsanev@users.noreply.github.com>
2021-02-24 14:01:08 +0200

    added new project

---
+++ AutoMapper/MusicX/MusicX/StarUp.cs
@@ -0,0 +1,43 @@
+﻿using AutoMapper;
+using MusicX.Data.Models;
+using Newtonsoft.Json;
+using System;
+using System.Linq;
+
+namespace MusicX
+{
+    public class StarUp
+    {
+        static void Main()
+        {
+            var config = new MapperConfiguration(cfg =>
+            {
+                cfg.CreateMap<Artist,ArtistWithSongs>();
+            });
+
+            var mapper = config.CreateMapper();
+
+            var db = new MusicXContext();
+
+            var artist = db.Artists.FirstOrDefault();
+
+            var artistViewModel = mapper.Map<ArtistWithSongs>(artist);
+
+
+
+            Print(artistViewModel);
+        }
+
+        public static void Print(object artists)
+        {
+            Console.WriteLine(JsonConvert.SerializeObject(artists, Formatting.Indented));
+        }
+
+        class ArtistWithSongs
+        {
+            public string Name { get; set; }
+
+            public int SongsCount { get; set; }
+        }
+    }
+}
