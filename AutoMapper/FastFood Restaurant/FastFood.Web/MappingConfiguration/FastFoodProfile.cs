5a7cbd4d4f945c351b09539b194f94399cdb9274
Yavor Tsanev <69981325+YavorTsanev@users.noreply.github.com>
2021-02-25 19:26:32 +0200

    refactoring

--- AutoMapper/FastFood Restaurant/FastFood.Web/MappingConfiguration/FastFoodProfile.cs
+++ AutoMapper/FastFood Restaurant/FastFood.Web/MappingConfiguration/FastFoodProfile.cs
@@ -14,10 +14,12 @@
             this.CreateMap<CreatePositionInputModel, Position>()
                 .ForMember(x => x.Name, y => y.MapFrom(s => s.PositionName));
 
-
+            this.CreateMap<Position, PositionsAllViewModel>();
 
             //Catalog
             this.CreateMap<CreateCategoryInputModel, Category>().ForMember(c => c.Name, opt => opt.MapFrom(m => m.CategoryName));
+
+            this.CreateMap<Category, CategoryAllViewModel>();
         }
     }
 }
