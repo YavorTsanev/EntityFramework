bfd8edbfd5094930d8da73f7fb44f610226fa0fc
Yavor Tsanev <69981325+YavorTsanev@users.noreply.github.com>
2021-03-02 17:22:25 +0200

    refactoring

---
+++ EXERCISE JSON PROCESSING/ProductShop/ProductShop/StartUp.cs
@@ -0,0 +1,43 @@
+﻿using AutoMapper;
+using Newtonsoft.Json;
+using ProductShop.Data;
+using ProductShop.Models;
+using System;
+using System.IO;
+
+namespace ProductShop
+{
+    public class StartUp
+    {
+
+        static void Main(string[] args)
+        {
+            var db = new ProductShopContext();
+
+            
+
+            var usersJson = File.ReadAllText("../../../Datasets/users.json");
+
+            var result = ImportUsers(db, usersJson);
+
+            Console.WriteLine(result);
+        }
+
+        public static string ImportUsers(ProductShopContext context, string inputJson)
+        {
+
+            var users = JsonConvert.DeserializeObject<User[]>(inputJson);
+
+            context.AddRange(users);
+            context.SaveChanges();
+
+            return $"Successfully imported {users.Length}";
+
+        }
+
+        public static string ImportProducts(ProductShopContext context, string inputJson)
+        {
+
+        }
+    }
+}
