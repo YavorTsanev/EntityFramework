eb9a10bee6fa9beca1cdbe4e3fd5fb42c635dd83
Yavor Tsanev <69981325+YavorTsanev@users.noreply.github.com>
2021-03-08 19:38:42 +0200

    added XML Procesing

---
+++ XML PROCESING/XML Demo/XML Demo/DTO/Car.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Xml.Serialization;
+
+namespace XML_Demo.DTO
+{
+    [XmlType("car")]
+    public partial class Car
+    {
+
+        private string makeField;
+
+        private string modelField;
+
+        private ulong travelleddistanceField;
+
+        public string make
+        {
+            get
+            {
+                return this.makeField;
+            }
+            set
+            {
+                this.makeField = value;
+            }
+        }
+
+        public string model
+        {
+            get
+            {
+                return this.modelField;
+            }
+            set
+            {
+                this.modelField = value;
+            }
+        }
+
+        [System.Xml.Serialization.XmlElementAttribute("travelled-distance")]
+        public ulong travelleddistance
+        {
+            get
+            {
+                return this.travelleddistanceField;
+            }
+            set
+            {
+                this.travelleddistanceField = value;
+            }
+        }
+    }
+}
---
+++ XML PROCESING/XML Demo/XML Demo/StartUp.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Xml;
+using System.Xml.Linq;
+using System.Linq;
+using System.Xml.Serialization;
+using System.IO;
+using XML_Demo.DTO;
+
+namespace XML_Demo
+{
+    class StartUp
+    {
+        static void Main(string[] args)
+        {
+            var xmlSerializer = new XmlSerializer(typeof(Car[]),new XmlRootAttribute("cars"));
+
+            var cars = (Car[])xmlSerializer.Deserialize(File.OpenRead("Datasets/cars.xml"));
+
+            xmlSerializer.Serialize(File.OpenWrite("../../../Datasets/carsXML.xml"),cars);
+        }
+    }
+
+
+}
