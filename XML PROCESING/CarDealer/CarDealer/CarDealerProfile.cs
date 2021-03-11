514457967ed2576bf9b51ffe21d028bed8a59b3c
Yavor Tsanev <69981325+YavorTsanev@users.noreply.github.com>
2021-03-11 13:21:15 +0200

    added new project

---
+++ XML PROCESING/CarDealer/CarDealer/CarDealerProfile.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+using CarDealer.Dtos.Import;
+using CarDealer.Models;
+
+namespace CarDealer
+{
+    public class CarDealerProfile : Profile
+    {
+        public CarDealerProfile()
+        {
+            CreateMap<ImportSupplierDto, Supplier>();
+
+            CreateMap<ImportPartDto, Part>();
+        }
+    }
+}
---
+++ XML PROCESING/CarDealer/CarDealer/StartUp.cs
@@ -0,0 +1,57 @@
+﻿using AutoMapper;
+using CarDealer.Data;
+using CarDealer.Dtos.Import;
+using CarDealer.Models;
+using ProductShop;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace CarDealer
+{
+    public class StartUp
+    {
+        private static readonly MapperConfiguration config = new MapperConfiguration(cfg => cfg.AddProfile<CarDealerProfile>());
+        static IMapper mapper = config.CreateMapper();
+
+        public static void Main(string[] args)
+        {
+            var db = new CarDealerContext();
+            //db.Database.EnsureDeleted();
+            //db.Database.EnsureCreated();
+
+            var xmlStr = File.ReadAllText("../../../Datasets/parts.xml");
+            var result = ImportParts(db, xmlStr);
+
+            Console.WriteLine(result);
+        }
+
+        public static string ImportSuppliers(CarDealerContext context, string inputXml)
+        {
+            var supplierDtos = XmlConverter.Deserializer<ImportSupplierDto>(inputXml, "Suppliers");
+
+            var suppliers = mapper.Map<List<Supplier>>(supplierDtos);
+
+            context.Suppliers.AddRange(suppliers);
+
+            context.SaveChanges();
+
+            return $"Successfully imported {suppliers.Count}";
+        }
+
+        public static string ImportParts(CarDealerContext context, string inputXml)
+        {
+
+            var partDtos = XmlConverter.Deserializer<ImportPartDto>(inputXml, "Parts").Where( p => context.Suppliers.Select(x => x.Id).Contains(p.SupplierId)).ToList();
+
+            var parts = mapper.Map<List<Part>>(partDtos);
+
+            context.Parts.AddRange(parts);
+
+            context.SaveChanges();
+
+            return $"Successfully imported {parts.Count}";
+        }
+    }
+}
