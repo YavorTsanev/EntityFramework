aab9e0b0b7f655690411d0a5965f829d8e240b4b
Yavor Tsanev <69981325+YavorTsanev@users.noreply.github.com>
2021-03-15 13:00:39 +0200

    create ef models

---
+++ Best Practices And Architecture/Real Estate/RealEstates.Data/RealEstateDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RealEstates.Data
+{
+    public class RealEstateDbContext: DbContext
+    {
+    }
+}
