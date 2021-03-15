5acb2fb58993ea22d2bd6ac6c1fb07de8421caa3
Yavor Tsanev <69981325+YavorTsanev@users.noreply.github.com>
2021-03-15 20:21:04 +0200

    add files

--- Best Practices And Architecture/Real Estate/RealEstates.Importer/Program.cs
+++ Best Practices And Architecture/Real Estate/RealEstates.Importer/Program.cs
@@ -9,7 +9,19 @@ namespace RealEstates.Importer
         {
             var jsonString = File.ReadAllText("imot.bg-raw-data-2020-07-23.json");
 
-            
+
         }
+
+        //"Url": "https://www.imot.bg/pcgi/imot.cgi?act=5&adv=1a159436674429522&slink=5p50ib&f1=1",
+        //"Size": 12,
+        //"Floor": 1,
+        //"TotalFloors": 5,
+        //"District": "град София, Стрелбище",
+        //"Year": 2022,
+        //"Type": "1-СТАЕН",
+        //"BuildingType": "Тухла",
+        //"Price": 17590
+
+        public class 
     }
 }
