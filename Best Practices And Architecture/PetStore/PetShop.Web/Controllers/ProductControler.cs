8e7c7809a671662c24e36a862188a5ccd4fe6292
Yavor Tsanev <69981325+YavorTsanev@users.noreply.github.com>
2021-03-19 15:21:25 +0200

    create PetStore.Web

---
+++ Best Practices And Architecture/PetStore/PetShop.Web/Controllers/ProductControler.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using PetStore.Services.Interfaces;
+
+namespace PetShop.Web.Controllers
+{
+    public class ProductControler : Controller
+    {
+        private readonly IProductService _productService;
+
+        public ProductControler(IProductService productService)
+        {
+            _productService = productService;
+        }
+        public IActionResult Index()
+        {
+            return RedirectToAction("All");
+        }
+
+        public IActionResult All()
+        {
+            var products = _productService.GetAll();
+
+            return View(products);
+        }
+    }
+}
