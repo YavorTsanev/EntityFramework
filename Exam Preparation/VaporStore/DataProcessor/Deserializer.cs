2e7ba3687c6dce1135aaf383374b03b0b62bae69
Yavor Tsanev <69981325+YavorTsanev@users.noreply.github.com>
2021-03-30 19:51:12 +0300

    implement Deserializer Class

--- Exam Preparation/VaporStore/DataProcessor/Deserializer.cs
+++ Exam Preparation/VaporStore/DataProcessor/Deserializer.cs
@@ -1,4 +1,11 @@
-﻿namespace VaporStore.DataProcessor
+﻿using System.Globalization;
+using System.Linq;
+using System.Text;
+using Newtonsoft.Json;
+using VaporStore.Data.Models;
+using VaporStore.DataProcessor.Dto.Import;
+
+namespace VaporStore.DataProcessor
 {
 	using System;
     using System.Collections.Generic;
@@ -8,8 +15,46 @@
 	public static class Deserializer
 	{
 		public static string ImportGames(VaporStoreDbContext context, string jsonString)
-		{
-			return "TODO";
+        {
+            var sb = new StringBuilder();
+
+            var gamesDto = JsonConvert.DeserializeObject<List<GameImportDto>>(jsonString);
+
+            foreach (var item in gamesDto)
+            {
+                if (!IsValid(item) || item.Tags.Count == 0)
+                {
+                    sb.AppendLine("Invalid Data");
+					continue;
+                }
+
+                var game = new Game
+                {
+                    Name = item.Name,
+                    Price = item.Price,
+                    ReleaseDate = DateTime.Parse(item.ReleaseDate, CultureInfo.InvariantCulture),
+                    Developer = context.Developers.FirstOrDefault(d => d.Name == item.Developer) ?? new Developer { Name = item.Developer },
+                    Genre = context.Genres.FirstOrDefault(g => g.Name == item.Genre) ?? new Genre { Name = item.Genre }
+                };
+
+
+                foreach (var tagg in item.Tags)
+                {
+                    var tag = context.Tags.FirstOrDefault(t => t.Name == tagg) ?? new Tag {Name = tagg};
+
+                    game.GameTags.Add(new GameTag
+                    {
+                        Tag = tag
+                    });
+                }
+
+                context.Games.Add(game);
+                context.SaveChanges();
+                sb.AppendLine($"Added {item.Name} ({item.Genre}) with {item.Tags.Count} tags");
+                
+            }
+
+			return sb.ToString().TrimEnd();
 		}
 
 		public static string ImportUsers(VaporStoreDbContext context, string jsonString)
---
+++ Exam Preparation/VaporStore/DataProcessor/Dto/Import/GameImportDto.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+using VaporStore.Data.Models;
+
+namespace VaporStore.DataProcessor.Dto.Import
+{
+    public class GameImportDto
+    {
+        [Required]
+        public string Name { get; set; }
+
+        [Range(typeof(decimal), "0", "79228162514264337593543950335")]
+        public decimal Price { get; set; }
+
+        [Required]
+        public string ReleaseDate { get; set; }
+
+        [Required]
+        public string Developer { get; set; }
+
+        [Required]
+        public string Genre { get; set; }
+
+        [Required]
+        public List<string> Tags { get; set; }
+
+       
+    }
+
+    //    {
+    //    "Price": 0,
+    //    "ReleaseDate": "2013-07-09",
+    //    "Developer": "Valid Dev",
+    //    "Genre": "Valid Genre",
+    //    "Tags": ["Valid Tag"]
+    //},
+
+
+
+}
